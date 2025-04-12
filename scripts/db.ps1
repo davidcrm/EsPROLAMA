66f36e1bfb51761de0c9e4bf5d4703efc2e6f08c
psuarezdev <pablotablero@gmail.com>
2025-04-12 18:59:27 +0100

    Fix: scripts/db.ps1

--- scripts/db.ps1
+++ scripts/db.ps1
@@ -1,16 +1,32 @@
-# Habilitar salida de errores
 $ErrorActionPreference = "Stop"
 
-Write-Host "🔄 Actualizando apt e instalando pgloader en el contenedor db..."
-docker compose exec db bash -c "apt update -y && apt install pgloader -y"
+$containerName = "elama_postgres"
+$containerExists = docker ps -a --format "{{.Names}}" | Where-Object { $_ -eq $containerName }
 
-Write-Host "🔄 Ejecutando migraciones en el contenedor web..."
-docker compose exec web bash -c "python manage.py migrate"
+if (-not $containerExists) {
+    Write-Host "⚠️ El contenedor '$containerName' no existe. Creándolo..."
+    docker run -d --name $containerName -p 5432:5432 -e POSTGRES_USER=elama -e POSTGRES_PASSWORD=uhu.2024 -e POSTGRES_DB=esprolama postgres:16
+} else {
+    $isRunning = docker ps --format "{{.Names}}" | Where-Object { $_ -eq $containerName }
 
-Write-Host "🔄 Copiando db.sqlite3 al contenedor db..."
-docker compose cp db.sqlite3 db:/tmp/db.sqlite3
+    if (-not $isRunning) {
+        Write-Host "▶️ El contenedor '$containerName' existe pero no está corriendo. Iniciándolo..."
+        docker start $containerName
+    } else {
+        Write-Host "✅ El contenedor '$containerName' ya está corriendo."
+    }
+}
+
+Write-Host "🔄 Actualizando apt e instalando pgloader en el contenedor $containerName..."
+docker exec $containerName bash -c "apt update -y && apt install pgloader -y"
+
+Write-Host "🔄 Ejecutando migraciones..."
+python manage.py migrate
+
+Write-Host "🔄 Copiando db.sqlite3 al contenedor $containerName..."
+docker cp db.sqlite3 ${containerName}:/tmp/db.sqlite3
 
 Write-Host "🔄 Ejecutando pgloader para migrar datos desde SQLite a PostgreSQL..."
-docker compose exec db bash -c "pgloader sqlite:///tmp/db.sqlite3 postgresql://elama:uhu.2024@localhost/esprolama"
+docker exec $containerName bash -c "pgloader sqlite:///tmp/db.sqlite3 postgresql://elama:uhu.2024@localhost/esprolama"
 
 Write-Host "✅ Proceso completado."

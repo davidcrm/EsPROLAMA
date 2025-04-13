fee20dca28a233c76cd6958653d0c8ac1b8db8ab
psuarezdev <pablotablero@gmail.com>
2025-04-13 13:00:30 +0100

    Fix: consulta del dashboard

--- elama/views/autoevaluacion.py
+++ elama/views/autoevaluacion.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 from elama.models import Autoevaluacion
 
 def mis_autoevaluaciones(request: HttpRequest):
-    autoevaluaciones = Autoevaluacion.objects.filter(usuario_id=request.user.id).order_by("fecha_hora")
+    autoevaluaciones = (Autoevaluacion.objects
+                        .filter(usuario_id=request.user.id, grupo_id__isnull=True)
+                        .order_by("fecha_hora"))
     return render(request, 'elama/dashboard.html', {
         'autoevaluaciones': autoevaluaciones
     })

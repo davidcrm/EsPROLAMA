e47bc6e60648249de354eb3a50e8f14d51078e96
albertoarr <albertoarranzromano@gmail.com>
2025-04-13 13:12:07 +0100

    UPDATE: Autoevaluaciones grupales

--- elama/views/grupal.py
+++ elama/views/grupal.py
@@ -2,21 +2,26 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpRequest
 from django.shortcuts import render
-
-from elama.models import Autoevaluacion
-from elama.models.grupo import Grupo
+from elama.models import Grupo, Autoevaluacion
 
 
 @login_required
 def crear_grupal(request:HttpRequest):
     usuario_autenticado = User.objects.get(pk=request.user.id)
-    grupos = Grupo.objects.filter(responsable_id=usuario_autenticado.id)
-
+    grupos_supervisados = Grupo.objects.filter(responsable_id=usuario_autenticado.id)
+    grupales_no_supervisadas = (Autoevaluacion.objects
+        .filter(
+            usuario_id=usuario_autenticado.id,
+            grupo_id__isnull=False,
+        ).exclude(
+            grupo__responsable_id=usuario_autenticado.id,
+        )
+    )
 
     return render(request,
         'elama/grupal.html',
         {
-            'grupos': grupos,
-
+            'grupos_supervisados': grupos_supervisados,
+            'grupales_no_supervisadas': grupales_no_supervisadas
          }
     )

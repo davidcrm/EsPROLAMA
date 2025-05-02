aa536d70a8d5a25e16cdda4c61a827a81f0821a1
psuarezdev <pablotablero@gmail.com>
2025-05-02 12:25:27 +0100

    Update: esprolama/settings.py

--- esprolama/settings.py
+++ esprolama/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+LOGIN_URL = '/users/login/'
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
